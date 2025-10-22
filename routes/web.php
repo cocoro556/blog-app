9c18647056fb632007b16d332c6d2067f0456383
cocoro556 <yuto.nakayama0@gmail.com>
2025-10-22 18:42:19 +0900

    setup blog app basic structure

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ContactController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/register', function () {
+    abort(403);
+});
+Route::post('/register', function () {
+    abort(403);
+});
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
